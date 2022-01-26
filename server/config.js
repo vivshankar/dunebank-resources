39a7c3757e230d321d93529a593218599d80c4f0
Vivek Shankar <viv.shankar@gmail.com>
2022-01-26 13:22:24 +0800

    bug: Removed TPP_MAP

--- server/config.js
+++ server/config.js
@@ -4,6 +4,5 @@ require('dotenv').config();
 exports.Config = {
     tenantUrl    : process.env.TENANT_URL,
     clientId     : process.env.CLIENT_ID,
-    clientSecret : process.env.CLIENT_SECRET,
-    tppMap       : JSON.parse(process.env.TPP_MAP),
+    clientSecret : process.env.CLIENT_SECRET
 };
